a5a77bcf16367c7907fbc1fcdbd3f570a289ad09
Marc Trösken <marctroesken@Marcs-MBP.fritz.box>
2020-01-05 08:34:09 +0100

    Initial commit

---
+++ database/migrations/2020_01_04_194220_create_projects_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id')->index();
+            $table->unsignedInteger('customer_id')->index();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->integer('total');
+            $table->enum('payment_provider', ['ratepay', 'paypal']);
+            $table->timestamp('payed_at');
+            $table->string('payment_reference');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
